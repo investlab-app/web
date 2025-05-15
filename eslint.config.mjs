5c626a07ce313b2f5cdeb42e93d65cfbf1ebe56b
mkapica <mikolajkapica7@gmail.com>
2025-05-15 23:20:07 +0200

    run eslint only for source and test directory

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,6 +6,8 @@ import pluginRouter from "@tanstack/eslint-plugin-router";
 
 export default [
   ...tseslint.config(eslint.configs.recommended, tseslint.configs.recommended),
-
   ...pluginRouter.configs["flat/recommended"],
+  {
+    files: ["src/**/*", "__test__/**/*"],
+  },
 ];
