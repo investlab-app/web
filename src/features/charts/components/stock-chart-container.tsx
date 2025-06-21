57ee0f51d4ab3ecceaf2582bdb8d294720842076
mkapica <mikolajkapica7@gmail.com>
2025-06-21 14:13:00 +0200

    format

--- src/features/charts/components/stock-chart-container.tsx
+++ src/features/charts/components/stock-chart-container.tsx
@@ -117,7 +117,7 @@ export const StockChartContainer = ({ ticker }: StockChartProps) => {
     changeInterval('1h');
     return () => {
       cleanup();
-    }
+    };
   }, [changeInterval, cleanup]);
 
   return (
