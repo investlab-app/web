5607c3b301fed5864f0e79eea832492a60237280
mkapica <mikolajkapica7@gmail.com>
2025-06-22 23:47:48 +0200

    fix bad error msg

--- src/features/charts/components/chart-error-message.tsx
+++ src/features/charts/components/chart-error-message.tsx
@@ -4,7 +4,7 @@ export const ChartErrorMessage = ({ message }: { message?: string }) => {
   const { t } = useTranslation();
   return (
     <div className="h-32 flex items-center justify-center text-center">
-      <p>{message || t('common.chart_error')}</p>
+      <p>{message || t('common.error_loading_data')}</p>
     </div>
   );
 };
