c8cf5f29f4cb75637454afa44a40ac8c0dfd4675
Anna Sidor <annasidor00@gmail.com>
2025-10-08 16:29:15 +0200

    save: sidebar ui

--- src/features/flows/nodes/rule/rule-node.tsx
+++ src/features/flows/nodes/rule/rule-node.tsx
@@ -2,7 +2,7 @@ import { memo } from 'react';
 import { RuleNodeUI } from '../../components/nodes-ui/rule-node-ui';
 import type { ReactNode } from 'react';
 import type { Node, NodeProps } from '@xyflow/react';
-import type { CommandNodeEvaluator,  } from '../../types/command-node-evaluator';
+import type { CommandNodeEvaluator } from '../../types/command-node-evaluator';
 
 export type RuleNode = Node<
   {
@@ -12,13 +12,11 @@ export type RuleNode = Node<
 >;
 
 export const RuleNode = memo((props: NodeProps<RuleNode>) => {
-  return (
-   <RuleNodeUI id={props.id} children={props.data.children} />
-  );
+  return <RuleNodeUI id={props.id} children={props.data.children} />;
 });
 
 export const RuleNodeEvaluator: CommandNodeEvaluator = {
-    evaluate() {
-        return {a: "b"};
-    }
-}
+  evaluate() {
+    return { a: 'b' };
+  },
+};
--- src/features/flows/nodes/rule/trigger/price-changes-node.tsx
+++ src/features/flows/nodes/rule/trigger/price-changes-node.tsx
@@ -1,6 +1,7 @@
 import { memo } from 'react';
-import { useUpdateNodeInternals, } from '@xyflow/react';
-import type {   Node,  NodeProps } from '@xyflow/react';
+import { useUpdateNodeInternals } from '@xyflow/react';
+import type { Node, NodeProps } from '@xyflow/react';
+import type { CommandNodeEvaluator } from '@/features/flows/types/command-node-evaluator';
 import { PriceChangesNodeUI } from '@/features/flows/components/nodes-ui/price-changes-node-ui';
 
 export type PriceChangesNode = Node<
@@ -12,25 +13,27 @@ export type PriceChangesNode = Node<
 >;
 
 export const PriceChangesNode = memo((props: NodeProps<PriceChangesNode>) => {
-
-const updateNodeInternals = useUpdateNodeInternals();
+  const updateNodeInternals = useUpdateNodeInternals();
   return (
-
-      <PriceChangesNodeUI
-        id={props.id}
-        value={props.data.value}
-        direction={props.data.direction}
-        onValueChange={(val) => {
-            props.data.value = val;
-            updateNodeInternals(props.id);
-
-        }}
-        onDirectionChange={(dir) => {
-            console.log("dir", dir);
-            props.data.direction = dir;
-            updateNodeInternals(props.id);
-        }}
-      />
-
+    <PriceChangesNodeUI
+      id={props.id}
+      value={props.data.value}
+      direction={props.data.direction}
+      onValueChange={(val) => {
+        props.data.value = val;
+        updateNodeInternals(props.id);
+      }}
+      onDirectionChange={(dir) => {
+        console.log('dir', dir);
+        props.data.direction = dir;
+        updateNodeInternals(props.id);
+      }}
+    />
   );
 });
+
+export const PriceChangeNodeEvaluator: CommandNodeEvaluator = {
+  evaluate(node, ) {
+    return { type: 'PriceChange', value: node.data.value, direction: node.data.direction};
+  },
+};
