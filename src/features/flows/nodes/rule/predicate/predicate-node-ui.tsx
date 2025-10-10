d1be74d1a77a95474849830d0bff45acc4e8848a
Anna Sidor <annasidor00@gmail.com>
2025-10-10 12:47:57 +0200

    save: another node

--- src/features/flows/nodes/rule/predicate/predicate-node-ui.tsx
+++ src/features/flows/nodes/rule/predicate/predicate-node-ui.tsx
@@ -5,11 +5,12 @@ import type { ReactNode } from 'react';
 interface PredicateNodeUIProps {
   children?: ReactNode;
   id: string;
+  connectionsLen?: number;
 }
 
-export function PredicateNodeUI({ children, id }: PredicateNodeUIProps) {
+export function PredicateNodeUI({ children, id, connectionsLen }: PredicateNodeUIProps) {
   return (
-    <RuleNodeUI id={id} className="bg-[var(--node-predicate)]">
+    <RuleNodeUI id={id} className="bg-[var(--node-predicate)]" connectionsLen={connectionsLen}>
       {children}
     </RuleNodeUI>
   );
