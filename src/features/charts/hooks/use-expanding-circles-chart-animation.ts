0e3be13aea0e0a51f3c4ca6e4b4a73c850d761d7
mkapica <mikolajkapica7@gmail.com>
2025-11-07 05:54:25 +0100

    save

--- src/features/charts/hooks/use-expanding-circles-chart-animation.ts
+++ src/features/charts/hooks/use-expanding-circles-chart-animation.ts
@@ -28,6 +28,9 @@ export function useExpandingCirclesChartAnimation({
 
     const chartInstance = chartRef.current.getEchartsInstance();
 
+    // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+    if (!chartInstance) return;
+
     // Animation config
     const expandDurationMs = 800;
     const startTs = Date.now();
