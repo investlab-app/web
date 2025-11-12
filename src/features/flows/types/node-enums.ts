07b003d83a5cfca0c7223bdea5a0296cae514232
Anna Sidor <annasidor00@gmail.com>
2025-11-12 12:31:10 +0100

    refactor: remove some enums

--- src/features/flows/types/node-enums.ts
+++ src/features/flows/types/node-enums.ts
@@ -1,54 +1,64 @@
-export enum BuySellAction {
-  Buy = 'buy',
-  Sell = 'sell',
-}
+export const BuySellAction = {
+  Buy: 'buy',
+  Sell: 'sell',
+} as const;
+export type BuySellAction = (typeof BuySellAction)[keyof typeof BuySellAction];
 
-export enum NotificationType {
-  Email = 'email',
-  Push = 'push',
-}
+export const NotificationType = {
+  Email: 'email',
+  Push: 'push',
+} as const;
+export type NotificationType = (typeof NotificationType)[keyof typeof NotificationType];
 
-export enum ComparisonDirection {
-  Greater = 'greater',
-  Lesser = 'less',
-}
+export const ComparisonDirection = {
+  Greater: 'greater',
+  Lesser: 'less',
+} as const;
+export type ComparisonDirection = (typeof ComparisonDirection)[keyof typeof ComparisonDirection];
 
-export enum TrendDirection {
-  Risen = 'risen',
-  Fell = 'fell',
-}
+export const TrendDirection = {
+  Risen: 'risen',
+  Fell: 'fell',
+} as const;
+export type TrendDirection = (typeof TrendDirection)[keyof typeof TrendDirection];
 
-export enum PositionDirection {
-  Above = 'above',
-  Below = 'below',
-}
+export const PositionDirection = {
+  Above: 'above',
+  Below: 'below',
+} as const;
+export type PositionDirection = (typeof PositionDirection)[keyof typeof PositionDirection];
 
-export enum TimeUnit {
-  Hour = 'hour',
-  Day = 'day',
-  Week = 'week',
-  Month = 'month',
-}
+export const TimeUnit = {
+  Hour: 'hour',
+  Day: 'day',
+  Week: 'week',
+  Month: 'month',
+} as const;
+export type TimeUnit = (typeof TimeUnit)[keyof typeof TimeUnit];
 
-export enum ShortTimeUnit {
-  Hour = 'hour',
-  Day = 'day',
-}
+export const ShortTimeUnit = {
+  Hour: 'hour',
+  Day: 'day',
+} as const;
+export type ShortTimeUnit = (typeof ShortTimeUnit)[keyof typeof ShortTimeUnit];
 
-export enum TransactionAction {
-  Bought = 'bought',
-  Sold = 'sold',
-}
+export const TransactionAction = {
+  Bought: 'bought',
+  Sold: 'sold',
+} as const;
+export type TransactionAction = (typeof TransactionAction)[keyof typeof TransactionAction];
 
-export enum PriceDirection {
-  Over = 'over',
-  Under = 'under',
-}
+export const PriceDirection = {
+  Over: 'over',
+  Under: 'under',
+} as const;
+export type PriceDirection = (typeof PriceDirection)[keyof typeof PriceDirection];
 
-export enum IndicatorType {
-  RollingAvg = 'rolling_avg',
-  Other = 'other',
-}
+export const IndicatorType = {
+  RollingAvg: 'rolling_avg',
+  Other: 'other',
+} as const;
+export type IndicatorType = (typeof IndicatorType)[keyof typeof IndicatorType];
 
 export function getEnumValues<T extends Record<string, string>>(
   enumObj: T
