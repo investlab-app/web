92d468383170bb3f93b0bfc02bef330fbc477dc9
Anna Sidor <annasidor00@gmail.com>
2025-11-13 17:22:08 +0100

    refactor: pretty crumb

--- src/routes/_authed/strategies/$flowId.tsx
+++ src/routes/_authed/strategies/$flowId.tsx
@@ -3,13 +3,25 @@ import { ReactFlowProvider } from '@xyflow/react';
 import AppFrame from '@/features/shared/components/app-frame';
 import { DnDProvider } from '@/features/flows/utils/dnd-context';
 import { FlowsBoard } from '@/features/flows/components/flows-board';
+import { graphLangRetrieveOptions } from '@/client/@tanstack/react-query.gen';
 
 export const Route = createFileRoute('/_authed/strategies/$flowId')({
   component: RouteComponent,
-  loader: ({ params: { flowId } }) => {
+  loader: async ({ params: { flowId }, context: { queryClient } }) => {
+    try {
+      const data = await queryClient.ensureQueryData(
+        graphLangRetrieveOptions({
+          path: {
+            id: flowId,
+          },
+        })
+      );
+      return { crumb: data.name || `${flowId}` };
+    } catch {
     return {
       crumb: `${flowId}`,
     };
+  }
   },
 });
 
