eb9a7aec0380178c1b4ea7845c807d9fcf3286fa
mkapica <mikolajkapica7@gmail.com>
2025-11-26 19:20:05 +0100

    debug

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,7 @@ import {
 } from '@tanstack/react-query';
 import { StrictMode, useEffect } from 'react';
 import { PostHogProvider } from 'posthog-js/react';
-import { ClerkLoaded, useAuth, useClerk } from '@clerk/clerk-react';
+import { useAuth, useClerk } from '@clerk/clerk-react';
 import { useTranslation } from 'react-i18next';
 import { routeTree } from './routeTree.gen';
 import { WSProvider } from './features/shared/providers/ws-provider.tsx';
